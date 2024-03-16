c2bef6392135783e5b8b121eced9322eab642abc
samimnif <samimnif@icloud.com>
2024-03-16 19:02:25 -0400

    initial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,29 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class Main {
+    private final String url = "jdbc:postgresql://localhost:5432/University";
+    private final String user = "postgres";
+    private final String password = "3005";
+    public Main() {
+    }
+
+    public void getAllStudents(){
+
+    }
+
+    public void addStudent(String first_name, String last_name, String email, String enrollment_date){
+
+    }
+
+    public void updateStudentEmail(int student_id, String new_email){
+
+    }
+
+    public void deleteStudent(int student_id){
+
+    }
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/University";
-        String user = "postgres";
-        String password = "3005";
+
 
         try {
             Class.forName("org.postgresql.Driver");
